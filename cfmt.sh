8dc5f5a5c40a77f0b7460d03ac581752f2d975f4
Randy J. Spaulding <rspauldi@amd.com>
2025-03-12 14:04:29 -0700

    cfmt

--- cfmt.sh
+++ cfmt.sh
@@ -2,13 +2,31 @@
 # Runs clang-format on all applicable files in MIOpen repo.
 # From https://github.com/ROCm/MIOpen/wiki/How-to-format-code
 
-find . -iname '*.h' \
-    -o -iname '*.hpp' \
-    -o -iname '*.cpp' \
-    -o -iname '*.h.in' \
-    -o -iname '*.hpp.in' \
-    -o -iname '*.cpp.in' \
-    -o -iname '*.cl' \
-| grep -v -E '(build/)|(install/)' \
-| xargs -n 1 -P $(nproc) -I{} -t clang-format-12 -style=file {} -i 2>/dev/null
+CLANG_CMD=clang-format-12
 
+if ! command -v ${CLANG_CMD} 2>&1 >/dev/null
+then
+    echo "Error! ${CLANG_CMD} could not be found"
+    exit 1
+fi
+
+if [ $# == 0 ]; then
+    find . -iname '*.h' \
+     -o -iname '*.hpp' \
+     -o -iname '*.cpp' \
+     -o -iname '*.h.in' \
+     -o -iname '*.hpp.in' \
+     -o -iname '*.cpp.in' \
+     -o -iname '*.cl' \
+   | grep -v -E '(build/)|(install.*/)' \
+   | xargs -n 1 -P $(nproc) -I{} -t ${CLANG_CMD} -style=file {} -i 2>/dev/null
+else
+    find . -iname '*.h' \
+     -o -iname '*.hpp' \
+     -o -iname '*.cpp' \
+     -o -iname '*.h.in' \
+     -o -iname '*.hpp.in' \
+     -o -iname '*.cpp.in' \
+     -o -iname '*.cl' \
+   | grep -v -E '(build/)|(install.*/)'
+fi
